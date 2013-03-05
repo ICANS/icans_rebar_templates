226c9d5017c2444085c86eb470f221768d44bfd5
Administrator <bjoern.kortuemm@icans-gmbh.com>
2013-03-05 20:32:47 +0100

    Fix: bin/dev_console.sh, paths corrected

--- _icans_default/bin/dev_console.sh
+++ _icans_default/bin/dev_console.sh
@@ -4,4 +4,4 @@ NODE="-name {{projectid}}@$(hostname --fqd)"
 
 cd $BASEDIR 
 
-erl $NODE -args_file $BASEDIR/config/{{projectid}}.node.args -pa $BASEDIR/deps/*/ebin $BASEDIR/ebin $BASEDIR/apps/*/deps/*/ebin $BASEDIR/apps/*/ebin -run {{projectid}} start
+erl $NODE -args_file $BASEDIR/config/{{projectid}}.node.args -pa $BASEDIR/deps/*/ebin $BASEDIR/ebin $BASEDIR/src/apps/*/deps/*/ebin $BASEDIR/src/apps/*/ebin -run {{projectid}} start
